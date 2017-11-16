e7e7d2ad9e52081ae28793c07673eb70bdae7764
lucarin91 <lucarin91@gmail.com>
2017-11-16 17:39:35 +0100

    update go group and add java group

--- data/examples/sockshop/sockshop-app/scripts/orders/start.sh
+++ data/examples/sockshop/sockshop-app/scripts/orders/start.sh
@@ -2,8 +2,7 @@
 
 # # java -Djava.security.egd=file:/dev/urandom -jar /carts/target/carts.jar --port=$INPUT_PORT
 # java -Djava.security.egd=file:/dev/urandom -jar $INPUT_JAR --port=$INPUT_PORT #-db=cart-db
-export JAVA_OPTS=-Djava.security.egd=file:/dev/urandom
 
-java -Ddebug -jar /orders/target/orders.jar --port=$INPUT_PORT
+java -Ddebug -Djava.security.egd=file:/dev/urandom -jar /orders/target/orders.jar --port=$INPUT_PORT
 
 # java -jar $INPUT_JAR --port=$INPUT_PORT --shipping_endpoint=$INPUT_SHIPPING --payment_endpoint=$INPUT_PAYMENT
--- data/examples/sockshop/sockshop-app/scripts/shipping/start.sh
+++ data/examples/sockshop/sockshop-app/scripts/shipping/start.sh
@@ -3,7 +3,7 @@
 
 # export JAVA_OPTS="-Djava.security.egd=file:/dev/urandom"
 
-/usr/local/bin/java.sh -jar $INPUT_JAR --port=$INPUT_PORT
+java -Djava.security.egd=file:/dev/urandom -jar $INPUT_JAR --port=$INPUT_PORT
 
 
-echo $JAVA_OPTS
+# echo $JAVA_OPTS
