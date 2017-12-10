4b81d6368d835f28f8cb06b444ada80b1fbc2554
lucarin91 <lucarin91@gmail.com>
2017-12-10 17:43:15 +0100

    fix test issue

--- data/examples/thinking-app/thinking/scripts/gui/configure.sh
+++ data/examples/thinking-app/thinking/scripts/gui/configure.sh
@@ -7,3 +7,9 @@ echo apiPort='"'$INPUT_APIPORT'"' >> $TARGET
 echo apiResource='"'$INPUT_APIRESOURCE'"' >> $TARGET
 
 cat $TARGET
+
+# add index.html that redirect to thoughts.html page.
+echo '<html>
+<head><meta http-equiv="refresh" content="0; url=/thoughts.html"/></head>
+<body><p><a href="/thoughts.html">Redirect</a></p></body>
+</html>' > /thinking-gui/public/index.html
