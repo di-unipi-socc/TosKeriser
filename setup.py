bb2b3fda082ba56508d23d34c503896e7573bf6e
lucarin91 <lucarin91@gmail.com>
2018-05-28 18:45:21 +0200

    fix python2 compatibility

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -64,7 +65,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['tosca-parser', 'six', 'requests', 'ruamel.yaml'],
+    install_requires=['cmd2<=0.8.6',  # BUGFIX: Added to avoide conflict in the
+                                      # version of cmd2 inside tosca_parser.
+                      'tosca-parser', 'six', 'requests', 'ruamel.yaml'],
+
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
